d6f63916f420b615226ccbd5086b08a2baf3b1d1
Artur Matos <arturrtutu@gmail.com>
2024-03-21 14:13:03 -0300

    commit inicial.

---
+++ src/artur/matos/mod8.java
@@ -0,0 +1,26 @@
+package artur.matos;
+
+public class mod8 {
+
+	public static void main(String[] args) {
+		System.out.println("Calculadora de média.");
+		System.out.println("Calcule a média de 4 notas.");
+		 
+		int nota1 =10;
+		int nota2 =6;
+		int nota3 =7;
+		int nota4 =5;
+		
+		float media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		
+		System.out.println("Sua primeira nota é "+ nota1);
+		System.out.println("Sua primeira nota é "+ nota2);
+		System.out.println("Sua primeira nota é "+ nota3);
+		System.out.println("Sua primeira nota é "+ nota4);
+		System.out.println("Sua média é "+ media);
+	}
+
+}
+
+	
